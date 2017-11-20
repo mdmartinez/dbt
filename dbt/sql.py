7924ad879f9cb1fee36b77098f59ef71c39cdcf9
Connor McArthur <cmcarthur@rjmetrics.com>
2017-11-20 13:17:57 -0500

    work with create view

--- dbt/sql.py
+++ dbt/sql.py
@@ -4,6 +4,7 @@ import sqlparse
 
 class TokenType:
     DML = 'Token.Keyword.DML'
+    CTE = 'Token.Keyword.CTE'
 
     @classmethod
     def match(cls, token, _type):
@@ -19,6 +20,10 @@ class TokenType:
         return cls.match(token, cls.DML)
 
     @classmethod
+    def is_cte(cls, token):
+        return cls.match(token, cls.CTE)
+
+    @classmethod
     def is_identifier_list(cls, token):
         return isinstance(token, sqlparse.sql.IdentifierList)
 
@@ -29,6 +34,7 @@ def parse_sql(sql):
 
 
 CTE_REGEX = re.compile('(?:with\s+)?(?P<name>\S*)\s+as\s+\((?P<sql>.*)\)', re.S)  # noqa
+CREATE_AS_REGEX = re.compile('(?P<name>\S*)\s+as\s+with(?P<sql>.*)', re.S)  # noqa
 
 
 def parse_cte_sql(sql):
@@ -45,34 +51,19 @@ def get_ctes(sql):
 
     # ctes are tuples of (identifier, dml)
     result = {'ctes': [], 'dml': None}
+
     in_cte = False
+    global_dml = []
 
-    # split the sql into (pre, dml)
-    #   pre == ctes
-    #   dml == select * from ...
-    # iterate through cte(s) and convert to tuple representation (identifier, dml)
-    #   recurse
-    # intermediate query representation
-    #   { 'ctes': (('identifier', 'dml',), ...), 'dml': '...' }
-    # join the query back together
+    for token in statement:
+        if TokenType.is_cte(token) and not in_cte:
+            in_cte = True
 
-    dml_index = None
+        elif TokenType.is_dml(token) and in_cte:
+            in_cte = False
+            global_dml.append(token.value)
 
-    for index, token in enumerate(statement):
-        if TokenType.is_dml(token):
-            dml_index = index
-            break
-
-    if dml_index is None:
-        return {'ctes': [], 'dml': sql}
-
-    ctes = statement[0:dml_index]
-
-    dml = statement[dml_index:]
-    result['dml'] = ''.join([d.value for d in dml])
-
-    for token in ctes:
-        if TokenType.is_identifier(token):
+        elif TokenType.is_identifier(token) and in_cte:
             groups = parse_cte_sql(token.value)
             if groups is None:
                 continue
@@ -82,19 +73,43 @@ def get_ctes(sql):
             result['ctes'] = result['ctes'] + parsed['ctes']
             result['ctes'].append((identifier, parsed['dml'],))
 
-        elif TokenType.is_identifier_list(token):
+        elif TokenType.is_identifier_list(token) and in_cte:
             for identifier_token in token.get_identifiers():
                 identifier, dml = parse_cte_sql(identifier_token.value)
                 parsed = get_ctes(dml)
 
                 result['ctes'] += parsed['ctes']
-                result['ctes'] += (identifier, parsed['dml'])
+                result['ctes'].append((identifier, parsed['dml'],))
+
+        elif TokenType.is_identifier(token) and not in_cte:
+            create_as_result = CREATE_AS_REGEX.search(token.value)
+
+            if create_as_result is not None:
+                name, sql = create_as_result.groups()
+
+                global_dml.append('{} as'.format(name))
+
+                identifier, dml = parse_cte_sql(sql)
+                parsed = get_ctes(dml)
+
+                result['ctes'] += parsed['ctes']
+                result['ctes'].append((identifier, parsed['dml'],))
+            else:
+                global_dml.append(token.value)
+
+        else:
+            global_dml.append(token.value)
+
+    result['dml'] = ''.join(global_dml).strip()
 
     return result
 
 
 def hoist_ctes(sql):
     result = get_ctes(sql)
+
+    if not result['ctes']:
+        return sql
 
     return 'with {} {}'.format(
         ', '.join(['{} as ({})'.format(identifier, dml)
--- test/unit/test_sql.py
+++ test/unit/test_sql.py
@@ -10,3 +10,9 @@ class TestSqlManipulations(unittest.TestCase):
         expected = 'with b as (select * from s.b), a as (select * from b) select * from a'  # noqa
 
         self.assertEqual(dbt.sql.hoist_ctes(start), expected)
+
+    def test__hoist_ctes__from_create_view(self):
+        start = 'create view d as with a as (with b as (select * from s.b) select * from b) select * from a'  # noqa
+        expected = 'with b as (select * from s.b), a as (select * from b) create view d as select * from a'  # noqa
+
+        self.assertEqual(dbt.sql.hoist_ctes(start), expected)
