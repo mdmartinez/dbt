7dbfb0108df3f88bd1fb40d3c653527bdca172e3
Connor McArthur <cmcarthur@rjmetrics.com>
2017-11-29 12:08:33 -0500

    fix view hoisting

--- dbt/include/global_project/macros/adapters/azure_dw.sql
+++ dbt/include/global_project/macros/adapters/azure_dw.sql
@@ -1,9 +1,6 @@
 {% macro azure_dw__create_view_as(identifier, sql) -%}
-  {% set to_hoist %}
-    create view {{ adapter.quote(schema) }}.{{ adapter.quote(identifier) }} as
-    {{ sql }}
-  {% endset %}
-  {{ hoist_ctes(to_hoist) }}
+  create view {{ adapter.quote(schema) }}.{{ adapter.quote(identifier) }} as
+  {{ hoist_ctes(sql) }}
 {% endmacro %}
 
 {% macro azure_dw__create_table_as(temporary, identifier, sql) -%}
